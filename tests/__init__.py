075833f49a5170e3440c6a07fc418b595a3f6f9f
Nathanael Lu <lunathanael@gmail.com>
2025-01-08 20:50:14 -0500

    Launch

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,11 @@
+import unittest
+from timeit import default_timer as timer
+
 import gymnasium as gym
 import balatro_gym
 import numpy as np
 import random
 env = gym.make("Balatro-v0", render_mode="text")
-
-# Clear the contents of cool.txt
-with open("cool.txt", "w") as f:
-    pass
 
 
 def runner():
@@ -21,16 +20,20 @@ def runner():
     selected_cards = random.sample(actual_hand.cards, played_cards)
     sorted_cards = sorted(selected_cards, key=lambda x: int(x), reverse=True)
 
-    with open("cool.txt", "a") as f:
-        print(sorted_cards, file=f)
-
     for card in selected_cards:
         playing_hand[int(card)] = 1
 
     playing_hand = np.append(playing_hand, [0])  # Add action type (play=0)
     obs, reward, terminated, truncated, info = env.step(playing_hand)
     if reward == -10 or terminated:
-        assert False
+        self.fail()
     # env.render()
-for i in range(100_000):
-    runner()
+
+    
+class TestBalatroGame(unittest.TestCase):
+    def test_runner(self):
+        for i in range(100_000):
+            runner()
+
+if __name__ == "__main__":
+    unittest.main()
